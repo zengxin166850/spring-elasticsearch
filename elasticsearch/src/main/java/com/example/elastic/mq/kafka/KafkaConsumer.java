6b5eb817480b3acf0174ef86a4535383393fc0d2
zengxin <1445216422@qq.com>
2018-11-15 14:50:33 +0800

    调整topic的分区数量

--- elasticsearch/src/main/java/com/example/elastic/mq/kafka/KafkaConsumer.java
+++ elasticsearch/src/main/java/com/example/elastic/mq/kafka/KafkaConsumer.java
@@ -17,9 +17,43 @@ public class KafkaConsumer {
     @Autowired
     KafkaTemplate<String,String> kafkaTemplate;
 
-    @KafkaListener(topics = {"test001"})
-    public void listen(ConsumerRecord<String,String> record){
-        logger.info("消费者监听到来自test001的消息.....");
+    @KafkaListener(topics = {"test001"},groupId = "group1")
+    public void g1consumer1(ConsumerRecord<String,String> record){
+        logger.info("消费者consumer1(group1)监听到来自test001的消息.....");
+        Optional<String> msg = Optional.ofNullable(record.value());
+
+        if(msg.isPresent()){
+            System.out.println(msg.get());
+            logger.info("----------record:"+record);
+            logger.info("----------msg:"+msg);
+            logger.info("------offset:"+record.offset());
+        }
+    }
+    @KafkaListener(topics = {"test001"},groupId = "group1")
+    public void g1consumer2(ConsumerRecord<String,String> record){
+        logger.info("消费者consumer2(group1)监听到来自test001的消息.....");
+        Optional<String> msg = Optional.ofNullable(record.value());
+        if(msg.isPresent()){
+            System.out.println(msg.get());
+            logger.info("----------record:"+record);
+            logger.info("----------msg:"+msg);
+            logger.info("------offset:"+record.offset());
+        }
+    }
+    @KafkaListener(topics = {"test001"},groupId = "group2")
+    public void g2consumer1(ConsumerRecord<String,String> record){
+        logger.info("消费者consumer2(group2)监听到来自test001的消息.....");
+        Optional<String> msg = Optional.ofNullable(record.value());
+        if(msg.isPresent()){
+            System.out.println(msg.get());
+            logger.info("----------record:"+record);
+            logger.info("----------msg:"+msg);
+            logger.info("------offset:"+record.offset());
+        }
+    }
+    @KafkaListener(topics = {"test001"},groupId = "group2")
+    public void g2consumer2(ConsumerRecord<String,String> record){
+        logger.info("消费者consumer2(group2)监听到来自test001的消息.....");
         Optional<String> msg = Optional.ofNullable(record.value());
         if(msg.isPresent()){
             System.out.println(msg.get());
--- elasticsearch/src/main/java/com/example/elastic/mq/kafka/KafkaProducer.java
+++ elasticsearch/src/main/java/com/example/elastic/mq/kafka/KafkaProducer.java
@@ -16,6 +16,15 @@ public class KafkaProducer {
     //仅用于测试
     public void send(){
         logger.info("生产者发送消息到test001......");
-        kafkaTemplate.send("test001","22222");
+        long millis = System.currentTimeMillis();
+        if (millis%2==0){
+//            kafkaTemplate.send("test001",2,"1","22222");
+            kafkaTemplate.send("test001",2,"1","11111");
+        }else{
+//            kafkaTemplate.send("test001",4,"2","33333");
+//            经试验，分区不能动态改变，只能在server.properties中进行固定设置。或者使用命令行针对某topic进行动态扩容(alter)
+            kafkaTemplate.send("test001",2,"2","33333");
+        }
+
     }
 }
